8f7d2d91b4cec66ad347cc2c1eb18d396eec99ae
宇都宮　誠 <textile-techno-0p@icloud.com>
2025-05-21 00:13:53 +0900

    APIを更新しました

--- server.js
+++ server.js
@@ -18,31 +18,33 @@ db.connect();
 
 // 提供API資料
 app.post('/getGoodsList', (req, res) => {
-  const { id } = req.body;
-  console.log("id: ", id);
-  db.query('SELECT * FROM sp_goods', (err, results) => {
+  console.log("/api/getGoodsList requested");
+  db.query('SELECT * FROM sp_goods ORDER BY goods_id DESC', (err, results) => {
     if (err) return res.status(500).send(err);
     res.json(results);
   });
 });
 app.post('/addGood', (req, res) => {
-  const { user_name, } = req.body;
-  const sql = `INSERT INTO sp_goods (user_name, mail_address, password, created, modified)
-    VALUES (?, ?, ?, now(), now());`
-  db.query(sql, [user_name], (err, results) => {
+  console.log("/api/addGood requested");
+  const { goods_name } = req.body;
+  const sql = `INSERT INTO sp_goods (goods_name, add_time, upd_time)
+    VALUES (?, UNIX_TIMESTAMP(NOW()), UNIX_TIMESTAMP(NOW()));`
+  db.query(sql, [goods_name], (err, results) => {
     if (err) return res.status(500).send(err);
     res.json(results);
   });
 });
 app.post('/updateGood', (req, res) => {
-  const { user_name, goods_id} = req.body;
-  const sql = `UPDATE sp_goods SET user_name = ?, mail_address = ? WHERE goods_id = ?;`
-  db.query(sql, [user_name, goods_id], (err, results) => {
+  console.log("/api/updateGood requested");
+  const { goods_name, goods_id} = req.body;
+  const sql = `UPDATE sp_goods SET goods_name = ? WHERE goods_id = ?;`
+  db.query(sql, [goods_name, goods_id], (err, results) => {
     if (err) return res.status(500).send(err);
     res.json(results);
   });
 });
 app.post('/deleteGood', (req, res) => {
+  console.log("/api/deleteGood requested");
   const { id } = req.body;
   db.query(`DELETE FROM sp_goods WHERE goods_id = ${id};`, (err, results) => {
     if (err) return res.status(500).send(err);
